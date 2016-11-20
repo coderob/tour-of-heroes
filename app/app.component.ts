4876050da9c32130a7412c1e3369ff0f8d5c734d
Robert Wilson <rwilson513@gmail.com>
2016-11-20 07:40:49 +0000

    Refactored to move Hero Details to its own component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-	id: number;
-	name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
 	{ id: 11, name: "Mr. Nice" },
@@ -33,13 +29,7 @@ const HEROES: Hero[] = [
 			</li>
 		</ul>
 
-		<div *ngIf="selectedHero">
-			<h2>{{ selectedHero.name }} details!</h2>
-			<div>
-				<label>id:</label> {{ selectedHero.id }}
-				<input [(ngModel)]="selectedHero.name" placeholder="name" />
-			</div>
-		</div>
+		<my-hero-detail [hero]="selectedHero"></my-hero-detail>
 	`,
 	styles: [`
 		.selected {
